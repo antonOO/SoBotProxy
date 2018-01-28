b2a24e4164abd8475fdb212b5ead2b59f3626fe5
kkb14183 <kkb14183@uni.strath.ac.uk>
2018-01-28 17:58:23 +0200

    fixed bm25

--- SO_proxy/views.py
+++ SO_proxy/views.py
@@ -3,6 +3,7 @@ import requests
 import re
 import json
 from gensim.summarization.bm25 import BM25
+from gensim import corpora
 from django.http import HttpResponse
 from django.conf import settings
 from django.views.decorators.csrf import csrf_protect
@@ -23,18 +24,28 @@ def split_to_passages(text):
     passages.append(text)
     return passages
 
+
 def get_search_data(search_query, programming_terms, intitle = ""):
     url_query = (search_query % {"tags" : programming_terms, "intitle" : intitle})
     response = requests.get(url_query)
     return json.loads(response.text)
 
+def strip_text(text):
+    text = text.lower()
+    text = re.sub('&.{3,6};', "", text)
+    return re.sub('[^0-9a-zA-Z ]+', "", text)
+
 
 def get_bm25_rankings(question_corpora, query_doc):
     parsed_query_doc = [word for word in query_doc.split()]
-    tokenized_qcorpora = [[word for word in question['title'].split()] for question in question_corpora]
+
+    parsed_qcorpora = [[word for word in strip_text(question['body']).split()] for question in question_corpora]
+    dictionary = corpora.Dictionary(parsed_qcorpora)
+    tokenized_qcorpora = [dictionary.doc2bow([word for word in strip_text(question['body']).split()]) for question in question_corpora]
+    tokenized_query_doc = dictionary.doc2bow(parsed_query_doc)
     bm25 = BM25(tokenized_qcorpora)
     average_idf = sum(map(lambda k: float(bm25.idf[k]), bm25.idf.keys())) / len(bm25.idf.keys())
-    return bm25.get_scores(parsed_query_doc, average_idf)
+    return bm25.get_scores(tokenized_query_doc, average_idf)
 
 
 def get_most_relevant_docs(rankings, documents):
@@ -48,19 +59,20 @@ def extract_possible_answers(relevant_docs):
     for doc in relevant_docs:
         for answer in doc["answers"]:
             passages.append(split_to_passages(answer["body"]))
-    shuffle(passages)
+    #shuffle(passages)
     return passages
 
 
 def get_answer(request):
     question = request.GET['question']
+    print(question)
     entities = eval(request.GET['entities'])
     intent = request.GET['intent']
     confidence = request.GET['confidence']
 
     print(str(entities))
 
-    similarity_terms = " ".join(word for word in question)
+    similarity_terms = " ".join(word for word in question.split())
     programming_terms = "; ".join([entity[0] for entity in entities if "programming" in entity[1]])
 
     #Query SO for similar information to the question.
@@ -73,13 +85,23 @@ def get_answer(request):
     passages = []
     question_corpora = []
 
+    i = 0
+    inds = []
     for item in json_search_data['items']:
+        if i == 0 :
+            print(strip_text(item["title"]))
+            print(item["title"])
         if "answers" in item and len(item['answers']) > 0:
             question_corpora.append(item)
+            inds.append(i)
+        i = i + 1
+
 
     scores = get_bm25_rankings(question_corpora, similarity_terms)
+    print(scores)
+    print(inds)
     relevant_docs = get_most_relevant_docs(scores, question_corpora)
-
+    print([doc["title"] for doc in relevant_docs])
     passages = extract_possible_answers(relevant_docs)
     print(len(passages))
     return HttpResponse(passages[0])
