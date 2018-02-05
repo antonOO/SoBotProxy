3ac303a705d3e2dbb37845811dd3a166ce7aa9b3
kkb14183 <kkb14183@uni.strath.ac.uk>
2018-02-05 21:16:57 +0200

    multiple answers + some documentation

--- SO_proxy/models.py
+++ SO_proxy/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class TrainingData(models.Model):
+    exact_match = models.IntegerField(blank=True, default=0)
+    term_overlap = models.FloatField(blank=True, default=0)
+    synonym_overlap = models.FloatField(blank=True, default=0)
+    language_model_score = models.FloatField(blank=True, default=0)
+    sentence_length = models.FloatField(blank=True, default=0)
+    sentence_location = models.FloatField(blank=True, default=0)
+    esa = models.FloatField(blank=True, default=0)
+    word_embedding = models.FloatField(blank=True, default=0)
+    entity_linking = models.FloatField(blank=True, default=0)
+    sentence_before = models.OneToOneField('self', related_name = "previous_sentence",  on_delete=models.SET_NULL, null=True)
+    sentence_after =  models.OneToOneField('self', related_name = "following_sentece",  on_delete=models.SET_NULL, null=True)
+    has_code = models.IntegerField(blank=True, default=0)
+    do_they_match = models.IntegerField(blank=True, default=0)
--- SO_proxy/views.py
+++ SO_proxy/views.py
@@ -10,6 +10,9 @@ from django.views.decorators.csrf import csrf_protect
 from random import shuffle
 
 
+'''
+
+'''
 def split_to_passages(text):
     '''passages = []
     while(True):
@@ -25,18 +28,54 @@ def split_to_passages(text):
     return passages
 
 
+'''
+Get the search data from SO
+
+params = specific search url (custom filter
+         as defined in SO) String, array of
+         Strings indicating tags -
+         programming terms, a String value
+         which appears in the retreaved
+         answers
+
+return = data from SO
+'''
 def get_search_data(search_query, programming_terms, intitle = ""):
     url_query = (search_query % {"tags" : programming_terms, "intitle" : intitle})
     print(url_query)
     response = requests.get(url_query)
     return json.loads(response.text)
 
+'''
+Remove unwanted symbols
+returned by SO
+
+params = String text
+
+return = stripped text
+'''
 def strip_text(text):
     text = text.lower()
     text = re.sub('&.{3,6};', "", text)
     return re.sub('[^0-9a-zA-Z ]+', "", text)
 
 
+'''
+Get bm25 rankings between a SO
+question and a query, depending
+on a question corpora
+
+params = question corpora - array
+        of tokenized docs, query
+        String, question part
+        indicating whether it is
+        the question title or body
+
+return = an array of floats - scores,
+        each index corresponding to
+        the doc in the question
+        corpora - its score
+'''
 def get_bm25_rankings(question_corpora, query_doc, question_part):
     parsed_query_doc = [word for word in query_doc.split()]
     parsed_qcorpora = [[word for word in strip_text(question[question_part]).split()] for question in question_corpora]
@@ -47,22 +86,40 @@ def get_bm25_rankings(question_corpora, query_doc, question_part):
     average_idf = sum(map(lambda k: float(bm25.idf[k]), bm25.idf.keys())) / len(bm25.idf.keys())
     return bm25.get_scores(tokenized_query_doc, average_idf)
 
+
+'''
+A bm25 scorer of the title
+and the body of a question
+in SO
+
+params = question corpora, query
+
+return = scores
+'''
 def get_bm25_combined(question_corpora, query_doc):
     scores_body = get_bm25_rankings(question_corpora, query_doc, "body")
     scores_title = get_bm25_rankings(question_corpora, query_doc, "title")
     return [scores_body[i] + scores_title[i] for i in range(len(scores_body))]
 
-def get_most_relevant_docs(rankings, documents):
-    max_rank = max(rankings)
-    highest_rank_indecies = [index for index, rank in enumerate(rankings) if rank == max_rank]
-    return [documents[i] for i in highest_rank_indecies]
+
+'''
+Compute
+'''
+def get_relevancy_sorted_docs(rankings, documents):
+    print(max(rankings))
+    index_rank_array = [(index, rank) for index, rank in enumerate(rankings)]
+    index_rank_array.sort(key=lambda index_rank: index_rank[1], reverse=True)
+    return [documents[index_rank[0]] for index_rank in index_rank_array]
 
 
-def extract_possible_answers(relevant_docs):
+def extract_possible_answers(relevant_docs, num_answers):
     passages = []
     for doc in relevant_docs:
         for answer in doc["answers"]:
-            passages.append(split_to_passages(answer["body"]))
+            if len(passages) == num_answers:
+                return passages
+            passages = passages + split_to_passages(answer["body"])
+
     #shuffle(passages)
     return passages
 
@@ -77,7 +134,7 @@ def get_answer(request):
     entities = eval(request.GET['entities'])
     intent = request.GET['intent']
     confidence = request.GET['confidence']
-    num_answers = int(request.get['num_answers'])
+    num_answers = int(request.GET['num_answers'])
 
     print(entities)
     '''
@@ -94,7 +151,7 @@ def get_answer(request):
     ''' '''
     json_search_data = get_search_data(settings.SIMILAR_QUESTION_FILTER, programming_terms, generic_query)
     if len(json_search_data['items']) == 0:
-        return HttpResponse("Cannot find an answer ...")
+        return HttpResponse("['Cannot find an answer ...']")
 
     max_score = 0
 
@@ -107,9 +164,9 @@ def get_answer(request):
                                                  #question
     scores = get_bm25_combined(question_corpora, generic_query)
     print(scores)
-    relevant_docs = get_most_relevant_docs(scores, question_corpora)
+    relevant_docs = get_relevancy_sorted_docs(scores, question_corpora)
     print(len(relevant_docs))
     print([doc["title"] for doc in relevant_docs])
-    passages = extract_possible_answers(relevant_docs)
-    print(len(passages))
-    return HttpResponse(passages[0])
+    passages = extract_possible_answers(relevant_docs, num_answers)
+
+    return HttpResponse(str(passages))
